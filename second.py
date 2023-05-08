41ec70e8917c48bedcac72b53758250e2ec724e1
kwonminsu2010 <112683597+kwonminsu2010@users.noreply.github.com>
2023-05-08 18:04:01 +0900

    Update second.py

--- second.py
+++ second.py
@@ -5,9 +5,7 @@ print('설명서를 보시고 시작해주세요.')
 
 text=input('공격 대상 입력하기 : ')
 print('공격대상은' ,text, '입니다.')
-print('해킹 진행중... SYN FLOOD 공격에 맞게 vpn을 준비해주세요.')
-
 if text.isupper() :
     print("공격대상을 소문자로 입력해주세요.")
 else :
-    print('동작 실패, 인터넷 연결을 확인해주세요.')
+    print('해킹 진행중... SYN FLOOD 공격에 맞게 vpn을 준비해주세요.')
